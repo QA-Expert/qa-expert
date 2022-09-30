3689281ef1c7932d0e136310d9f86b843d7b1a48
Andrei Surzhan <surzhan.a.y@gmail.com>
2022-09-29 23:33:12 -0400

    removed user controller

--- apps/api/src/modules/users/user.module.ts
+++ apps/api/src/modules/users/user.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.model';
 import { UserService } from './user.service';
-import { UserController } from './user.controller';
 import { UserResolver } from './user.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   providers: [UserService, UserResolver],
   exports: [UserService],
-  controllers: [UserController],
 })
 export class UserModule {}
