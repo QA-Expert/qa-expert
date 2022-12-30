17bb0c7440c0a36ebd7001e650ffb217e869bfe9
Andrei Surzhan <surzhan.a.y@gmail.com>
2022-12-29 21:47:35 -0500

    fixed issue with checking jwt

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,16 +2,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from './modules/config/config.service';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { execSync } = require('child_process');
 
-const cmd = `curl -s http://checkip.amazonaws.com || printf "0.0.0.0"`;
-const pubIp = execSync(cmd).toString().trim();
-
-console.log(`My public IP address is: ${pubIp}`);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(app.get(ConfigService).port);
 }
+
 bootstrap();
--- apps/web/utils/auth.ts
+++ apps/web/utils/auth.ts
@@ -4,5 +4,4 @@
  */
 export const isTokenExpired = (token: string) =>
   // TODO: Solve - A Node.js API is used (Buffer at line: 4) which is not supported in the Edge Runtime.
-  Date.now() >=
-  JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString()).exp * 1000;
+  Date.now() >= JSON.parse(atob(token.split('.')[1]).toString()).exp * 1000;
