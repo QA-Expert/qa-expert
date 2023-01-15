0417e20ddc3926b8cda5313a3c337b43efa0711e
Andrei Surzhan <surzhan.a.y@gmail.com>
2023-01-14 21:14:37 -0500

    Added completed courses section

---
+++ apps/web/src/components/card/card-image.tsx
@@ -0,0 +1,49 @@
+import CardMedia from '@mui/material/CardMedia';
+import { Box } from '../box/box';
+import Image from 'next/image';
+import { useTheme } from '@mui/material/styles';
+import { ReactNode } from 'react';
+
+type Props = {
+  children?: ReactNode;
+  alt: string;
+  width: number;
+  height: number;
+  breakPointWidth?: number;
+  src: string;
+};
+
+export function CardImage({
+  children,
+  src,
+  alt,
+  width,
+  height,
+  breakPointWidth,
+}: Props) {
+  const theme = useTheme();
+
+  return (
+    <CardMedia
+      sx={{
+        display: 'flex',
+        justifyContent: 'start',
+        position: 'relative',
+      }}
+    >
+      <Box
+        sx={{
+          width: width,
+          height: height,
+          [theme.breakpoints.down('md')]: {
+            width: width && !breakPointWidth ? width : breakPointWidth,
+          },
+        }}
+      >
+        <Image alt={alt} layout="fill" src={src} objectFit="cover" />
+      </Box>
+
+      {children}
+    </CardMedia>
+  );
+}
---
+++ apps/web/src/components/completed-courses-section/handlers.ts
@@ -0,0 +1,25 @@
+import { CourseProps } from '../../../pages/courses';
+
+export const getCoursesToShowBySectionWidth = (
+  width: number,
+  courses: CourseProps[],
+) => {
+  const remainingCoursesToShow = [...courses];
+  const maxNumberOfCardsInCurrentWidth = width
+    ? Math.round(width / 250)
+    : courses.length;
+  const remainingCoursesNumberToShow =
+    courses.length - maxNumberOfCardsInCurrentWidth;
+  const coursesToShow =
+    remainingCoursesNumberToShow < courses.length
+      ? remainingCoursesToShow.splice(
+          remainingCoursesNumberToShow,
+          courses.length - remainingCoursesNumberToShow,
+        )
+      : [];
+
+  return {
+    remainingCoursesToShow,
+    coursesToShow,
+  };
+};
